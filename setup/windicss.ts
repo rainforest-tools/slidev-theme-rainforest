d60f9ae36e7ab9cc060d961a8947d67bb87f7ccf
Rainforest Cheng <rainforestnick@gmail.com>
2021-12-09 14:50:40 +0800

    feat: add daisyui support

--- setup/windicss.ts
+++ setup/windicss.ts
@@ -1,14 +1,25 @@
 import { resolve } from "path";
 import { defineWindiSetup } from "@slidev/types";
 import { transform } from "windicss/helpers";
+import daisyColors from "daisyui/colors/index.js";
 
 export default defineWindiSetup(() => ({
   extract: {
     include: [resolve(__dirname, "../**/*.{vue,ts}")],
   },
+  theme: {
+    extend: {
+      colors: {
+        ...daisyColors,
+      },
+    },
+  },
   shortcuts: {
     // custom the default background
-    "bg-main": "bg-white text-[#181818] dark:(bg-[#121212] text-[#ddd])",
+    // "bg-main": "bg-white text-[#181818] dark:(bg-[#121212] text-[#ddd])",
   },
-  plugins: [transform("daisyui")],
+  daisyui: {
+    themes: ["valentine", "cyberpunk"],
+  },
+  plugins: [transform("@tailwindcss/typography"), transform("daisyui")],
 }));

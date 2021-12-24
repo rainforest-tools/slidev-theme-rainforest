5cc16e0f0f0f6d7305da744cd9161a3853caa81a
Rainforest Cheng <rainforestnick@gmail.com>
2021-12-24 21:52:16 +0800

    fix: fix import issue

--- global-bottom.vue
+++ global-bottom.vue
@@ -27,7 +27,7 @@ import {
   onMounted,
   getCurrentInstance,
 } from "vue";
-import * as themeChange from "theme-change";
+import { themeChange } from "theme-change";
 import useCurrentInstance from "./hooks/useCurrentInstance";
 
 export default defineComponent({
@@ -38,7 +38,7 @@ export default defineComponent({
       () => selected,
       (value) => globalProperties.$setThemeData(value)
     );
-    onMounted(() => themeChange.themeChange(false));
+    onMounted(() => themeChange(false));
     return { selected };
   },
 });
